e8cc2d99acbc87213d171c6d3104f3498e2c4b69
jtumms <john.tumminelli@tummsmedia.com>
2016-09-28 01:53:21 -0400

    Initial load - Contacts Desktop

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,29 @@
 package sample;
 
+import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import jodd.json.JsonSerializer;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import static java.awt.SystemColor.text;
 
 
-public class Controller implements Initializable {
+public class Controller implements Initializable, ChangeListener{
     @FXML
     TextField name;
 
@@ -26,14 +36,19 @@ public class Controller implements Initializable {
     @FXML
     ListView list;
 
+    @FXML
+    Button buttonAdd;
+
     ObservableList<Contact> contacts = FXCollections.observableArrayList();
 
-    public void addContact(){
+
+    public void addContact() throws IOException {
         String inputName = name.getText();
         String inputPhone = phone.getText();
         String inputEmail = email.getText();
         Contact contact = new Contact(inputName, inputPhone, inputEmail);
         contacts.add(contact);
+
         name.clear();
         phone.clear();
         email.clear();
@@ -49,6 +64,21 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         list.setItems(contacts);
+//        if (name.getText().isEmpty()){
+//            buttonAdd.setDisable(true);
+//        }
 
     }
+    @Override
+    public void changed(ObservableValue observable, Object oldValue, Object newValue){
+
+
+    }
+
+
+
+
+
+
+
 }
