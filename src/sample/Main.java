5d67467f5a44bbb640564665c7ba5547faee74cd
jtumms <john.tumminelli@tummsmedia.com>
2016-09-29 13:46:15 -0400

    WOrking !!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
+//import static sample.Controller.loadJson;
+
 public class Main extends Application {
 
     @Override
@@ -17,7 +21,10 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         launch(args);
+
+
+
     }
 }
